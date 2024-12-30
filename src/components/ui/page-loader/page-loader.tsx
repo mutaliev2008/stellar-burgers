cf827e3e9ddf6fbd2a926c3adc7b52f7f65578af
Магомед-Башир <mutalievm350@gmail.com>
2024-12-30 16:35:29 +0300

    stellar-burgers

--- src/components/ui/page-loader/page-loader.tsx
+++ src/components/ui/page-loader/page-loader.tsx
@@ -3,8 +3,6 @@ import { Preloader } from '../preloader';
 import styles from './page-loader.module.css';
 
 export const PageLoader = () => (
-  // const {} =
-
   <div className={styles.overlay}>
     <Preloader />
   </div>
